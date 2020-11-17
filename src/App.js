e8fad95a0d0494719714dacb03e555de80cd6d14
RX-78GP02A <mridley@knights.ucf.edu>
2020-11-17 18:52:26 -0500

    updated index.js & App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Switch, Route } from 'react-router-dom';
+import Navbar from './components/pages/Navbar';
+import Home from './components/pages/HomePage/Home';
+import About from './components/pages/AboutPage/About';
+import Recipes from './components/pages/RecipesPage/Recipez';
+import Profile from './components/pages/ProfilePage/Profile';
+import Footer from './components/pages/Footer/Footer';
+import Form from './components/pages/SignupPage/Form';
+import LoginForm from './components/pages/LoginPage/LoginForm'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/> 
+        <Route path='/About' component={About}/>
+        <Route path='/Recipes' component={Recipes}/>
+        <Route path='/Profile' component={Profile}/>
+        <Route path='/Form' component={Form}/>
+        <Route path='/LoginForm' component={LoginForm}/>
+      </Switch>
+      <Footer />
     </div>
   );
 }
