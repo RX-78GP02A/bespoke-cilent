5a70ac07e22f92ba1cb6a69ae0e0ba62d7bba531
RX-78GP02A <mridley@knights.ucf.edu>
2020-11-17 19:34:25 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Switch, Route } from 'react-router-dom';
 import Navbar from './components/pages/Navbar';
 import Home from './components/pages/HomePage/Home';
 import About from './components/pages/AboutPage/About';
-import Recipes from './components/pages/RecipesPage/Recipez';
+import Recipes from './components/pages/RecipesPage/Recipes';
 import Profile from './components/pages/ProfilePage/Profile';
 import Footer from './components/pages/Footer/Footer';
 import Form from './components/pages/SignupPage/Form';
