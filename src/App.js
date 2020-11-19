93d5363103b4fc511daf8caeb5a9d5b2a3ed1666
RX-78GP02A <mridley@knights.ucf.edu>
2020-11-19 18:42:26 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from 'react';
 import './App.css';
 import {Switch, Route } from 'react-router-dom';
 import Navbar from './components/pages/Navbar';
-import Home from './components/pages/HomePage/Home';
-import About from './components/pages/AboutPage/About';
-import Recipes from './components/pages/RecipesPage/Recipes';
-import Profile from './components/pages/ProfilePage/Profile';
-import Footer from './components/pages/Footer/Footer';
-import Form from './components/pages/SignupPage/Form';
-import LoginForm from './components/pages/LoginPage/LoginForm'
+
 
 
 
@@ -17,12 +11,6 @@ function App() {
     <div>
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home}/> 
-        <Route path='/About' component={About}/>
-        <Route path='/Recipes' component={Recipes}/>
-        <Route path='/Profile' component={Profile}/>
-        <Route path='/Form' component={Form}/>
-        <Route path='/LoginForm' component={LoginForm}/>
       </Switch>
       <Footer />
     </div>
