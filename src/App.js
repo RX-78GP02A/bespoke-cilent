be5de568935c3c629b201eb289891d37190ae0c4
RX-78GP02A <mridley@knights.ucf.edu>
2020-11-19 20:22:10 -0500

    created route paths for cilent

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 import {Switch, Route } from 'react-router-dom';
 import Navbar from './components/pages/Navbar';
-
-
+import Home from './components/pages/HomePage/Home';
+import About from './components/pages/AboutPage/About';
+import Profile from './components/pages/ProfilePage/Profile';
+import Recipes from './components/pages/Recipes/Recipes';
+import Footer from './components/pages/Footer/Footer';
+import Form from './components/pages/SignupPage/Form';
+import Login from './components/pages/Login/';
 
 
 function App() {
@@ -11,8 +16,14 @@ function App() {
     <div>
       <Navbar />
       <Switch>
-      <Route path='/' exact component={Home}/> 
+      <Route path='/' exact component={Home}/>
+      <Route path='/About' component={About}/>
+      <Route path='/Recipes' component={Recipes}/>
+      <Route path='/Profile' component={Profile}/>
+      <Route path='/Form' component={Form}/>
+      <Route path='/Login' component={Login}/> 
       </Switch>
+      <Footer />
     </div>
   );
 }
