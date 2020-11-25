c27674334d6f90c4e66043c370cf732419efbefd
RX-78GP02A <mridley@knights.ucf.edu>
2020-11-25 17:42:42 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Navbar from './components/pages/Navbar';
 import Home from './components/pages/Home/Home';
 import About from './components/pages/About/About';
