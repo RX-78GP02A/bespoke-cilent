295c497c901be53a6e2ba59e72c80f194c08c182
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-15 16:54:20 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `http://localhost:8080`,
-    // API_ENDPOINT: `https://stark-river-56520.herokuapp.com`,
+    // API_ENDPOINT: `http://localhost:8080`,
+     API_ENDPOINT: `https://stark-river-56520.herokuapp.com`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'blogful-client-auth-token',
   }
