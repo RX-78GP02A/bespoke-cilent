6f8a18638991b2a38a77642c50664ab7293f4d0a
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-15 15:15:02 -0500

    created Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./footer.css";
+import { Link } from "react-router-dom";
+
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+         
+          <div className="footer-link-items">
+            <h2>Contact Me</h2>
+            <a href="mailto:mridley@knights.ucf.edu?subject='Hello'">
+              Support
+            </a>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <a target = "_blank" href="https://www.linkedin.com/in/marcus-ridley/" >LinkedIn</a>
+
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              BESPOKE
+            </Link>
+          </div>
+          <small className="website-rights">©  Marcus Ridley Codes 2020</small>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
