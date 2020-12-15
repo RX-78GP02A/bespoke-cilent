7cbbc316b7b117ec41fda54689a82efd0fe6a5ac
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-15 14:15:58 -0500

    created Navbar.css

---
+++ src/components/ObjectStorage.js
@@ -0,0 +1,41 @@
+export const homeObjectA = {
+    mainBackground: false,
+    mainText: true,
+    mainTextDescription: true,
+    mainHeader: 'Cheers From Bespoke App',
+    headline: 'Cocktail Recipe Refernce Guide For The Avid Drink Enthusiast',
+    description: "Remember to always drink responsibly",
+    hasButton: false,
+    buttonLabel: '',
+    img: 'images/nollie_flip.jpg',
+    alt: ''
+}
+
+export const homeObjectB = {
+    mainBackground: false,
+    mainText: true,
+    mainTextDescription: true,
+    mainHeader: 'About Bespoke!',
+    headline: "It's Just Drinks" ,
+    description: "Bespoke was created for the cocktail enthusiast and bartender; it's a one stop shop that allows it's user to reference cocktails as well as add cocktail recipes for future use.",
+    buttonLabel: '',
+    img: 'images/jupiter_jazz.jpg',
+    alt: ''
+}
+
+export const homeObjectC = {
+    mainBackground: false,
+    mainText: true,
+    mainTextDescription: true,
+    mainHeader: 'Bespoke Profile',
+    headline: 'Create Your Profile',
+    description: 'Create your cocktail profile below.',
+    hasButton: true,
+    buttonLabel: 'Get Started',
+    img: 'images/trio.jpg',
+    alt: ''
+}
+
+
+
+
